dfdc67276056fe1eb4dcc8718ae3f9403fbc711a
Nick Alteen <ncalteen@github.com>
2023-09-20 17:01:18 -0400

    Add issue template parser

---
+++ src/parse.ts
@@ -0,0 +1,45 @@
+import { IssueFormTemplate, FormattedField } from './interfaces'
+import { formatKey } from './format'
+
+/**
+ * Parses the issue form template and returns a dictionary of fields
+ * @param template The issue form template
+ * @returns A dictionary of fields
+ */
+export async function parseTemplate(
+  template: IssueFormTemplate
+): Promise<{ [key: string]: FormattedField }> {
+  const fields: { [key: string]: FormattedField } = {}
+
+  for (const item of template.body) {
+    // Skip markdown fields
+    if (item.type === 'markdown') continue
+
+    // Convert the label to snake case. This is the heading in the issue body
+    // when the form is submitted, and is used by issue-ops/parser as the key.
+    const formattedKey: string = formatKey(item.attributes.label)
+
+    // Take the rest of the attributes and add them to the fields object
+    fields[formattedKey] = {
+      type: item.type,
+      required: item.validations?.required || false
+    }
+
+    if (item.type === 'dropdown') {
+      // These fields are only used by dropdowns
+      fields[formattedKey].default = item.attributes.default
+      fields[formattedKey].multiple = item.attributes.multiple || false
+      fields[formattedKey].options = item.attributes.options
+    }
+
+    if (item.type === 'checkboxes') {
+      // Checkboxes have a different options format than dropdowns
+      // Enforce false for required if not present
+      fields[formattedKey].options = item.attributes.options.map((x) => {
+        return { label: x.label, required: x.required || false }
+      })
+    }
+  }
+
+  return fields
+}
