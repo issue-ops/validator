d000f7e709a9804e73afb45d50062efddefcdf93
Nick Alteen <ncalteen@github.com>
2023-09-24 00:43:27 -0400

    Update validator script type

--- .github/validator/team.js
+++ .github/validator/team.js
@@ -1,23 +1,19 @@
 /**
- * Validation logic for GitHub Teams
- */
-import { Octokit } from '@octokit/rest'
-
-/**
  * Checks if a team exists
- * @param field The input field. This can be one of several types:
+ * @param {string | string[] | {label: string; required: boolean }} field The input field.
  *
+ * This can be one of several types:
  *  - A string, which is the value of the field (e.g. "my-team").
  *  - An array of strings, which are the values of the field (e.g. ["my-team"]).
  *  - An object with a `label` and `required` property. This is used for checkboxes.
  *
  *  You do not need to handle them all! It is up to the validator to determine
  *  which type(s) to expect, and how to handle them.
- * @returns An error message if validation fails, 'success' otherwise
+ * @returns {Promise<string>} An error message if validation fails, 'success' otherwise
  */
-export async function exists(
-  field: string | string[] | { label: string; required: boolean }
-): Promise<string> {
+export async function exists(field) {
+  const { Octokit } = require('@octokit/rest')
+
   // You will need to set any required environment variables in the GitHub
   // Actions workflow file that runs the validator action. This is how you can
   // specify inputs to the custom validators. In this case, you will need to
@@ -38,7 +34,7 @@ export async function exists(
     })
 
     return 'success'
-  } catch (error: any) {
+  } catch (error) {
     // If the team does not exist, return an error message
     if (error.status === 404) return `Team ${field} does not exist`
     // Otherwise, throw the exception
