57fa024028ec2abf01dcdb76a8a78af6e5ad0e22
Nick Alteen <ncalteen@github.com>
2024-09-24 09:47:19 -0400

    Rebuil dist

--- dist/index.js
+++ dist/index.js
@@ -649,7 +649,6 @@ function md5ii(a, b, c, d, x, s, t) {
 }
 
 var v3 = v35('v3', 0x30, md5);
-var v3$1 = v3;
 
 function v4(options, buf, offset) {
   options = options || {};
@@ -767,7 +766,6 @@ function sha1(bytes) {
 }
 
 var v5 = v35('v5', 0x50, sha1);
-var v5$1 = v5;
 
 var nil = '00000000-0000-0000-0000-000000000000';
 
@@ -785,9 +783,9 @@ var esmBrowser = /*#__PURE__*/Object.freeze({
 	parse: parse$4,
 	stringify: stringify$4,
 	v1: v1,
-	v3: v3$1,
+	v3: v3,
 	v4: v4,
-	v5: v5$1,
+	v5: v5,
 	validate: validate$1,
 	version: version
 });
